ca4519af8a29aa4d17580b7754e7c10249998114
Gustavo I. Chalco <gchalcoa@gmail.com>
2019-03-10 17:32:40 -0500

    Se realiza validacion del parametro a ingresar

--- server.py
+++ server.py
@@ -1,19 +1,20 @@
 from flask import Flask, request, jsonify
 import numpy
+from n_term_fibo import get_term_fibo
 
 app = Flask(__name__)
 
 @app.route('/nfibo/<n>', methods=['GET'])
 def get_n_fibo(n):
-    
-    m = int(n)
-    F = numpy.matrix([[1, 1], [1, 0]])
-    nfibo = (F ** ( m - 1 ))[0, 0]
 
-    res = int(nfibo)
+    if n.isdigit():
+        m = int(n)
+        res = get_term_fibo(m)
+        return jsonify({'mensaje': 'el n-esimo termino de la sucesion de Fibonacci',
+                        'resultado': res} ), 200
+    else:
+        return jsonify({'mensaje': 'El parametro ingresado no es un termino valido'} ), 500
 
-    return jsonify({'mensaje': 'el n-esimo termino de la sucesion de Fibonacci',
-                    'resultado': res} )
 
 
 if __name__ == '__main__':
